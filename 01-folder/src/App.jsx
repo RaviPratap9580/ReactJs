ed564bb72e7eae5a804d97c2519dbf3a098a59ca
Ravi Pratap Singh <ravisie170@gmail.com>
2026-01-02 14:58:24 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Guys!!!</h1>
+      <h3>Hello</h3>
+    </div>
+  )
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,7 @@
+// var h1=document.createElement('h1');
+// h1.innerHTML='Hello form Js';
+// document.body.appendChild(h1);
+
+// import a from './app.js';
+
+// console.log(a);
